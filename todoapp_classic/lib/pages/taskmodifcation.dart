e5a7177f1c1cabdda779ff46208026230534acfe
Lindokuhle Mashele <lmashele023b@student.wethinkcode.co.za>
2024-04-06 20:16:03 +0200

    Updated settings page

--- todoapp_classic/lib/pages/taskmodifcation.dart
+++ todoapp_classic/lib/pages/taskmodifcation.dart
@@ -1,41 +1,47 @@
-
-
 import 'package:flutter/material.dart';
-import 'package:hive_flutter/hive_flutter.dart';
-import 'package:provider/provider.dart';
-import 'package:todoapp_classic/componets/zoeTaskTracker.dart';
 
 class taskModification extends StatelessWidget {
-  static String id = "taskModificationPage";
-  const taskModification({super.key});
+  TextEditingController titleController;
+  TextEditingController descriptionController;
+  final Function callThisFunction;
+  taskModification({
+  required this.titleController,
+  required this.descriptionController,
+  required this.callThisFunction});
 
   @override
   Widget build(BuildContext context) {
-    TextEditingController zoeController = TextEditingController();
-    final _zoeCanYou = Hive.box("myDB");
-    final zoeDo = Provider.of<zoeTaskTracker>(context);
-
     return Scaffold(
-      appBar: AppBar(
-        title: Text("Edit page"),
-        centerTitle: true,
-      ),
-      floatingActionButton: FloatingActionButton(onPressed: (){
-        // zoeDo.writeToHive(zoeController.text);
-        if(zoeController.text.isNotEmpty){
-          _zoeCanYou.add(zoeController.text);
-        }
-        // ChangeNotifier();
-        Navigator.pop(context);
-      }),
-      body: SafeArea(child: Padding(padding: EdgeInsets.only(top:9,right: 10,left: 10),
-      child: TextField(
-        controller: zoeController,
-        decoration: InputDecoration(
-          hintText: "Task Name II"
+
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal:8.0),
+          child: Column(children: [
+            SizedBox(height: 20,),
+            TextField(controller: titleController,decoration: InputDecoration(hintText: "Title"),),
+            SizedBox(height: 30,),
+           
+            TextField(controller: descriptionController,maxLines: null,decoration: InputDecoration(hintText: "Description"),),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                   TextButton(onPressed: (){
+                  titleController.clear();
+                  descriptionController.clear();
+                  Navigator.of(context).pop();
+          
+                }, child: Text("Cancel")),
+                TextButton(onPressed: (){
+                 if(titleController.text.isNotEmpty && descriptionController.text.isNotEmpty){
+                   callThisFunction(titleController.text,descriptionController.text);
+                 }
+                  Navigator.of(context).pop();
+                }, child: Text("Save")),
+             
+              ],
+            )
+          ],),
         ),
-      ),
-      ),
       ),
     );
   }
